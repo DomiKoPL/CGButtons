325e885172c2871d4a6eb6c6e4051a6443feb28a
DomiKoPL <xkowal99@gmail.com>
2022-05-05 21:56:40 +0200

    version 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CGButtons
-Helper buttons for CodinGame
+
+## Installation
+
+1. Open Chrome
+2. Go to Settings -> Extensions
+3. Turn on Developer Mode
+4. Load unpacked [copy-extension](copy-extension)
+
+
+## Usage 
+
---
+++ copy-extension/contentscript.js
@@ -0,0 +1,86 @@
+const copyToClipboardAsync = str => {
+    if (navigator && navigator.clipboard && navigator.clipboard.writeText)
+        return navigator.clipboard.writeText(str);
+
+    return Promise.reject('The Clipboard API is not available.');
+}
+
+const copyNodeStyle = (sourceNode, targetNode) => {
+    const computedStyle = window.getComputedStyle(sourceNode);
+    for (const key of computedStyle) {
+        targetNode.style.setProperty(key, computedStyle.getPropertyValue(key), computedStyle.getPropertyPriority(key))
+    }
+}
+
+loading = setInterval(function () {
+    const header = document.getElementsByClassName('ide-header');
+
+    if (!header || header.length == 0) {
+        console.log('Header not loaded!');
+        return;
+    }
+
+    const title = document.getElementsByClassName("cg-ide-title");
+
+    if (!title || title.length == 0) {
+        console.log('Game title not loaded!');
+        return;
+    }
+
+
+    let button = document.createElement("Button");
+    let text = document.createTextNode("Copy stderr");
+
+    copyNodeStyle(title[0], button);
+
+    const rank = document.getElementsByClassName("rank-wrapper");
+
+    if (rank && rank.length > 0) {
+        // console.log("Rank not loaded!");
+        // Copy color from current League
+
+        // TODO: Sometimes rank could not be loaded
+
+        const rankStyle = window.getComputedStyle(rank[0]);
+        button.style.webkitTextFillColor = rankStyle.webkitTextFillColor;
+    }
+
+    button.onclick = function (event) {
+        console.log("Button clicked!");
+
+        let elements = document.getElementsByClassName('stderr');
+
+        if (elements.length == 0) {
+            return;
+        }
+
+        let content = "";
+
+        for (let turn = 0; turn < elements.length; turn++) {
+            let element = elements[turn];
+            /*
+                element.innerText sometimes have new lines symbols
+                and sometimes doesn't :/
+            */
+
+            // content += elements[turn].innerText + "\n";
+
+            for (let line of element.getElementsByClassName("outputLine")) {
+                content += line.innerText + "\n";
+            }
+        }
+
+        if (content.length > 0) {
+            copyToClipboardAsync(content);
+            alert(`Copied${content.length} lines.`);
+        }
+    }
+
+    button.appendChild(text);
+
+    header[0].appendChild(button);
+
+    console.log('Button is added!');
+
+    clearInterval(loading);
+}, 100); // Checks every 100ms(0.1s)
